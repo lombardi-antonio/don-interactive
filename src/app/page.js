feaa74014fb5e11c8015625c736aafc3286b4838
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2023-09-20 12:37:55 +0200

    Add analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 "use client";
 
 import Home from "./home/page";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Root() {
   return (
   <main>
     <Home />
+    <Analytics />
   </main>);
 }
