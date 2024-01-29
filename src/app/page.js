707bcaed40d29541128088d644772d7995520bac
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2024-01-29 11:37:25 +0100

    Add speed insights

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 
 import Home from "./home/page";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Root() {
   return (
   <main>
     <Home />
     <Analytics />
+    <SpeedInsights />
   </main>);
 }
