f2f714e512e536b994f1c5289570e889655127f4
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2024-02-01 15:27:36 +0100

    adjust socials reactivity

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -15,7 +15,6 @@ import bgElement01 from "/public/bgElement01.svg";
 import bgElement02 from "/public/bgElement02.svg";
 import bgElement03 from "/public/bgElement03.svg";
 
-
 const monomaniac = localFont({
   src: "../fonts/MonomaniacOne-Regular.ttf",
   fontFamily: "Monomaniac",
@@ -29,47 +28,64 @@ const rubik = localFont({
 export default function Home() {
   return (
     <main>
-      <div className={`${rubik.className} absolute w-full h-auto min-h-screen bg-repeat bg-[url('../../public/grid.png')]`}>
-        <div className="header grid grid-cols-6 w-full fixed blur-[200px]">
+      <div
+        className={`${rubik.className} absolute h-auto min-h-screen w-full bg-[url('../../public/grid.png')] bg-repeat`}
+      >
+        <div className="header fixed grid w-full grid-cols-6 blur-[200px]">
           <div className="terrazzo-main col-span-6 min-h-[512px] w-full animate-[spin_14s_linear_infinite]">
-            <Image src={bgElement00} alt="Terrazzo Background"/>
+            <Image src={bgElement00} alt="Terrazzo Background" />
           </div>
           <div className="terrazzo-end col-span-6 h-[512px] w-full animate-[spin_18s_linear_infinite]">
             <Image src={bgElement01} alt="Terrazzo Background" />
           </div>
           <div className="terrazzo-main col-span-6 h-0 w-full animate-[spin_10s_linear_infinite]">
-            <Image src={bgElement02} alt="Terrazzo Background"/>
+            <Image src={bgElement02} alt="Terrazzo Background" />
           </div>
           <div className="terrazzo-end col-span-6 h-[512px] w-full animate-[spin_12s_linear_infinite]">
-            <Image src={bgElement03} alt="Terrazzo Background"/>
+            <Image src={bgElement03} alt="Terrazzo Background" />
           </div>
-          <div className="terrazzo-main col-span-6 absolute top-20 right-50 h-0 w-full animate-[spin_16s_linear_infinite]">
-            <Image src={bgElement00} alt="Terrazzo Background" height={500} width={500}/>
+          <div className="terrazzo-main right-50 absolute top-20 col-span-6 h-0 w-full animate-[spin_16s_linear_infinite]">
+            <Image
+              src={bgElement00}
+              alt="Terrazzo Background"
+              height={500}
+              width={500}
+            />
           </div>
-          <div className="terrazzo-end col-span-6 absolute top-20 right-10 h-0 w-full animate-[spin_22s_linear_infinite]">
-            <Image src={bgElement01} alt="Terrazzo Background" height={500} width={500} />
+          <div className="terrazzo-end absolute right-10 top-20 col-span-6 h-0 w-full animate-[spin_22s_linear_infinite]">
+            <Image
+              src={bgElement01}
+              alt="Terrazzo Background"
+              height={500}
+              width={500}
+            />
           </div>
-          <div className="terrazzo-main col-span-6 absolute top-4 left-10 h-0 w-full animate-[spin_12s_linear_infinite]">
-            <Image src={bgElement02} alt="Terrazzo Background" height={500} width={500}/>
+          <div className="terrazzo-main absolute left-10 top-4 col-span-6 h-0 w-full animate-[spin_12s_linear_infinite]">
+            <Image
+              src={bgElement02}
+              alt="Terrazzo Background"
+              height={500}
+              width={500}
+            />
           </div>
-          <div className="terrazzo-end col-span-6 absolute top-50 right-30 h-0 w-full animate-[spin_10s_linear_infinite]">
-            <Image src={bgElement03} alt="Terrazzo Background" height={500} width={500}/>
+          <div className="terrazzo-end top-50 right-30 absolute col-span-6 h-0 w-full animate-[spin_10s_linear_infinite]">
+            <Image
+              src={bgElement03}
+              alt="Terrazzo Background"
+              height={500}
+              width={500}
+            />
           </div>
         </div>
 
-        <div className="main-card relative grid auto-cols-fr auto-rows-fr grid-cols-7 grid-rows-3 gap-6 h-full mx-auto max-w-full lg:max-w-[920px] xl:max-w-[2160px]">
+        <div className="main-card relative mx-auto grid h-full max-w-full auto-cols-fr auto-rows-fr grid-cols-7 grid-rows-3 gap-6 lg:max-w-[920px] xl:max-w-[2160px]">
           <div
             id="three-logo"
-            className="
-            animate-fade-in-model transition ease-in-out duration-[0.8s]
-            col-start-1 xl:col-start-5 col-span-full
-            row-start-2 row-span-1 md:row-start-1 md:row-span-full
-            h-[400px] md:h-auto md:mt-0 xl:h-full"
+            className="animate-fade-in-model col-span-full col-start-1 row-span-1 row-start-2 h-[400px] transition duration-[0.8s] ease-in-out md:row-span-full md:row-start-1 md:mt-0 md:h-auto xl:col-start-5 xl:h-full"
           >
             <LogoModel />
           </div>
-          <div className="col-start-1 col-span-full xl:col-span-4
-            row-start-1 row-span-1 mx-0 xs:mx-12 xl:mx-6 mt-6">
+          <div className="xs:mx-12 col-span-full col-start-1 row-span-1 row-start-1 mx-0 mt-6 xl:col-span-4 xl:mx-6">
             <Card header="DO∩ アントニオ" subheader="interactive">
               Welcome to DON interactive, the home of innovative and immersive
               media experiences. We are a team of passionate and creative
@@ -81,77 +97,97 @@ export default function Home() {
               Consider following us on:
               <br />
               <br />
-              <div className="flex flex-row space-x-2 justify-center">
-                <form action="https://www.instagram.com/don_interactive" target="_blank" rel="noopener noreferrer">
+              <div className="flex flex-row justify-center space-x-2">
+                <form
+                  action="https://www.instagram.com/don_interactive"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button
                     type="submit"
-                    className="text-center text-white hover:text-black
-                    scale-100 hover:scale-105 transform
-                    transition duration-300 bg-gradient-to-tr
-                    from-teal-500 via-indigo-500 to-rose-500
-                    hover:from-teal-300 hover:via-indigo-300 hover:to-rose-300
-                      font-medium rounded-full text-sm px-5 py-2.5 inline-flex items-center mr-2 mb-2"
+                    className="mb-2 mr-2 inline-flex scale-100 transform items-center rounded-full bg-gradient-to-tr from-teal-500 via-indigo-500 to-rose-500 px-5 py-2.5 text-center text-sm font-medium text-white transition duration-300 hover:scale-105 hover:from-teal-300 hover:via-indigo-300 hover:to-rose-300 hover:text-black"
                   >
-                    <Image src={instagramGlyph} alt="Instagram Glyph" height={24} width={24} className="mr-2 -ml-2" />
-                    Instagram
+                    <Image
+                      src={instagramGlyph}
+                      alt="Instagram Glyph"
+                      height={24}
+                      width={24}
+                      className="xs:-ml-2 xs:mr-2"
+                    />
+                    <p className="xs:inline hidden">Instagram</p>
                   </button>
                 </form>
-                <form action="https://github.com/lombardi-antonio" target="_blank" rel="noopener noreferrer">
+                <form
+                  action="https://github.com/lombardi-antonio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button
                     type="submit"
-                    className="text-center text-white hover:text-black
-                    scale-100 hover:scale-105 transform
-                    transition duration-300 bg-gradient-to-tr
-                    from-teal-500 via-indigo-500 to-rose-500
-                    hover:from-teal-300 hover:via-indigo-300 hover:to-rose-300
-                      font-medium rounded-full text-sm px-5 py-2.5 inline-flex items-center mr-2 mb-2"
+                    className="mb-2 mr-2 inline-flex scale-100 transform items-center rounded-full bg-gradient-to-tr from-teal-500 via-indigo-500 to-rose-500 px-5 py-2.5 text-center text-sm font-medium text-white transition duration-300 hover:scale-105 hover:from-teal-300 hover:via-indigo-300 hover:to-rose-300 hover:text-black"
                   >
-                    <Image src={githubGlyph} alt="Github Glyph" height={24} width={24} className="mr-2 -ml-2" />
-                    Github
+                    <Image
+                      src={githubGlyph}
+                      alt="Github Glyph"
+                      height={24}
+                      width={24}
+                      className="xs:-ml-2 xs:mr-2"
+                    />
+                    <p className="xs:inline hidden">Github</p>
                   </button>
                 </form>
-                <form action="https://www.reddit.com/r/don_interactive" target="_blank" rel="noopener noreferrer">
+                <form
+                  action="https://www.reddit.com/r/don_interactive"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button
                     type="submit"
-                    className="text-center text-white hover:text-black
-                    scale-100 hover:scale-105 transform
-                    transition duration-300 bg-gradient-to-tr
-                    from-teal-500 via-indigo-500 to-rose-500
-                    hover:from-teal-300 hover:via-indigo-300 hover:to-rose-300
-                      font-medium rounded-full text-sm px-5 py-2.5 inline-flex items-center mr-2 mb-2"
+                    className="mb-2 mr-2 inline-flex scale-100 transform items-center rounded-full bg-gradient-to-tr from-teal-500 via-indigo-500 to-rose-500 px-5 py-2.5 text-center text-sm font-medium text-white transition duration-300 hover:scale-105 hover:from-teal-300 hover:via-indigo-300 hover:to-rose-300 hover:text-black"
                   >
-                    <Image src={redditGlyph} alt="Reddit Glyph" height={24} width={24} className="mr-2 -ml-2" />
-                    Reddit
+                    <Image
+                      src={redditGlyph}
+                      alt="Reddit Glyph"
+                      height={24}
+                      width={24}
+                      className="xs:-ml-2 xs:mr-2"
+                    />
+                    <p className="xs:inline hidden">Reddit</p>
                   </button>
                 </form>
               </div>
             </Card>
           </div>
-          <div
-            className="col-start-1 col-span-full xl:col-start-3 xl:col-span-2
-            row-start-5 md:row-start-4 xl:row-start-2 row-span-2 md:row-span-2
-            ml:0 xs:ml-12 xl:ml-0 mr-0 xs:mr-12 xl:mr-6 mb-6 xl:mb-0
-          ">
-            <Card appImageSrc={bfos} header="Beats from Outer Space" textPosition="left" linkButtonUrl="/bfos">
-              Hello there, Terrarians! I am your mysterious radio host, Midnight,
-              and I have a story to share with you. It is the tale of a brave
-              space hero, who pilots the magnificent Vapor Falcon in a battle
-              against the body snatching aliens from outer space.
+          <div className="ml:0 xs:ml-12 xs:mr-12 col-span-full col-start-1 row-span-2 row-start-5 mb-6 mr-0 md:row-span-2 md:row-start-4 xl:col-span-2 xl:col-start-3 xl:row-start-2 xl:mb-0 xl:ml-0 xl:mr-6">
+            <Card
+              appImageSrc={bfos}
+              header="Beats from Outer Space"
+              textPosition="left"
+              linkButtonUrl="/bfos"
+            >
+              Hello there, Terrarians! I am your mysterious radio host,
+              Midnight, and I have a story to share with you. It is the tale of
+              a brave space hero, who pilots the magnificent Vapor Falcon in a
+              battle against the body snatching aliens from outer space.
             </Card>
           </div>
-          <div
-            className="col-start-1 xl:col-start-1 col-span-full xl:col-span-2
-            row-start-3 md:row-start-2 xl:row-start-2 row-span-2 md:row-span-2
-            ml:0 xs:ml-12 xl:ml-6 mr-0 xs:mr-12 xl:mr-0 mb-0
-          ">
-            <Card appImageSrc={moneyPenny} header="Money Penny" textPosition="left" linkButtonUrl="/money-penny">
+          <div className="ml:0 xs:ml-12 xs:mr-12 col-span-full col-start-1 row-span-2 row-start-3 mb-0 mr-0 md:row-span-2 md:row-start-2 xl:col-span-2 xl:col-start-1 xl:row-start-2 xl:ml-6 xl:mr-0">
+            <Card
+              appImageSrc={moneyPenny}
+              header="Money Penny"
+              textPosition="left"
+              linkButtonUrl="/money-penny"
+            >
               <p>
                 Money Penny ist deine persönliche Finanzbewertungs-App, die dir
-                dabei hilft, deine Finanzen intelligent und mühelos zu organisieren.
+                dabei hilft, deine Finanzen intelligent und mühelos zu
+                organisieren.
               </p>
               <br />
               <p>
-                Money Penny fragt dich nach deinem aktuellen Finanzstatus ab und erstellt dir einen Finanzbericht, der dir hilft, deine Finanzen zu verstehen und zu verbessern.
+                Money Penny fragt dich nach deinem aktuellen Finanzstatus ab und
+                erstellt dir einen Finanzbericht, der dir hilft, deine Finanzen
+                zu verstehen und zu verbessern.
               </p>
             </Card>
           </div>
