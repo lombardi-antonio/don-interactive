f32d1288afa66e2092057db609d0cf3272514064
Antonio Lombardi <A.Lombardi013@gmail.com>
2025-07-03 10:06:17 +0200

    remove empty g play button

--- src/app/bfos/page.js
+++ src/app/bfos/page.js
@@ -31,39 +31,23 @@ export default function Home() {
           "
         >
           <Card appImageSrc={bfos} header="Beats form Outer Space">
-          <div className="flex flex-row justify-center">
-              <a href="https://play.google.com/store/apps/details?id=org.doninteractive.beatsfromouterspace" target="_blank" rel="noopener noreferrer">
+            <div className="flex flex-row justify-center">
+              <a href="https://apps.apple.com/us/app/beats-from-outer-space/id6467766328" target="_blank" rel="noopener noreferrer">
                 <button
                   className="
-                    md:px-10
-                    scale-100 hover:scale-105 transform
-                    transition duration-300"
-                  type="submit"
-                >
-                  <Image
-                    src="/google-play-badge.png"
-                    height={264}
-                    width={800}
-                    alt="Google Play Store Button"
-                  />
-                </button>
-              </a>
-            <a href="https://apps.apple.com/us/app/beats-from-outer-space/id6467766328" target="_blank" rel="noopener noreferrer">
-              <button
-                className="
                   px-10 min-width-[256px]
                   scale-100 hover:scale-105 transform
                   transition duration-300"
-                type="submit"
-              >
-                <Image
-                  src="/Download_on_the_App_Store_Badge_blk.svg"
-                  height={264}
-                  width={740}
-                  alt="Apple App Store Button"
-                />
-              </button>
-            </a>
+                  type="submit"
+                >
+                  <Image
+                    src="/Download_on_the_App_Store_Badge_blk.svg"
+                    height={80}
+                    width={256}
+                    alt="Apple App Store Button"
+                  />
+                </button>
+              </a>
             </div>
             <p className="text-md text-left pt-2 pb-4">
               Hello there, Terrarians! I am your mysterious radio host,
@@ -95,20 +79,22 @@ export default function Home() {
               This is Midnight signing off.
             </p>
             <Button type="link" linkButtonUrl="https://github.com/lombardi-antonio/beats-from-outer-space">
-              <svg
-                className="w-4 h-4 mr-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              View in Github
+              <div className="flex items-center justify-center">
+                <svg
+                  className="w-4 h-4 mr-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                View in Github
+              </div>
             </Button>
           </Card>
         </div>
