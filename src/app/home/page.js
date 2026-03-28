34d1fef2bb5f568a22ff5ac8a8b715c179d417d7
Antonio Lombardi <A.Lombardi013@gmail.com>
2026-03-28 16:07:21 +0100

    add new phone frame component

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import Card from "@/app/components/vetro/Card";
+import Telaio from "@/app/components/vetro/Telaio";
 import TerrazzoShader from "@/app/components/vetro/TerrazzoShader";
 import Button from "../components/vetro/Button";
 
@@ -21,20 +22,25 @@ export default function Home() {
       <section className="main-card relative grid auto-cols-fr auto-rows-fr grid-cols-7 grid-rows-3 gap-6 py-16 h-full mx-auto max-w-full lg:max-w-[920px] xl:max-w-[2160px]" aria-labelledby="main-content">
         <article className="col-start-1 col-end-8 xl:col-start-2 xl:col-end-7 row-start-1 row-span-1">
           <Card header="DO∩ アントニオ" subheader="interactive" textPosition="left">
-            <p>
-              This is a mock business created by me to showcase my skills and passion
-              for all things interactive. Here you&#39;ll find examples of my work in
-              digital media, design, and development.
-              My goal is to demonstrate creativity, technical ability,
-              and a love for building engaging interactive experiences.
-            </p>
-            <div className="h-6"></div>
-            <Button type="info" linkButtonUrl="mailto:doninteract@gmail.com">
-              <div className="flex items-center justify-center text-black dark:text-white">
-                <span className="mr-2 text-xl">@</span>
-                contact me
-              </div>
-            </Button>
+            <div className="absolute top-0 -right-20 -rotate-6 pointer-events-none">
+              <Telaio src="/loadingScreenImage.png" />
+            </div>
+            <div className="pr-64">
+              <p>
+                This is a mock business created by me to showcase my skills and passion
+                for all things interactive. Here you&#39;ll find examples of my work in
+                digital media, design, and development.
+                My goal is to demonstrate creativity, technical ability,
+                and a love for building engaging interactive experiences.
+              </p>
+              <div className="h-6"></div>
+              <Button type="info" linkButtonUrl="mailto:doninteract@gmail.com">
+                <div className="flex items-center justify-center text-black dark:text-white">
+                  <span className="mr-2 text-xl">@</span>
+                  contact me
+                </div>
+              </Button>
+            </div>
           </Card>
         </article>
 
